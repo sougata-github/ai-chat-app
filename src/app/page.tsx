d529af3277bd63be949a7aa41b4c935a9dd8bd07
Sougata <sougatadas9874@gmail.com>
2025-06-09 12:45:01 +0530

    setup routes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
- <div>
-  hello
- </div>
-  );
+  return redirect("/chat");
 }
