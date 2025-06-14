b332565f8ab57dd3626d7693c154fdfe85656ac1
Sougata <sougatadas9874@gmail.com>
2025-06-14 13:10:10 +0530

    fix: type error server.tsx.

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -1,6 +1,7 @@
 import "server-only"; // <-- ensure this file cannot be imported from the client
 
 import { createHydrationHelpers } from "@trpc/react-query/rsc";
+import { headers } from "next/headers";
 import { cache } from "react";
 
 import { createCallerFactory, createTRPCContext } from "./init";
@@ -10,7 +11,16 @@ import { appRouter } from "./routers/_app";
 /* IMPORTANT: Create a stable getter for the query client that
              will return the same client during the same request.*/
 export const getQueryClient = cache(makeQueryClient);
-const caller = createCallerFactory(appRouter)(createTRPCContext);
+
+const caller = await (async () => {
+  const req = new Request("http://localhost", {
+    headers: await headers(),
+  });
+  const resHeaders = new Headers();
+  const ctx = await createTRPCContext({ req, resHeaders });
+  return createCallerFactory(appRouter)(ctx);
+})();
+
 export const { trpc, HydrateClient } = createHydrationHelpers<typeof appRouter>(
   caller,
   getQueryClient
