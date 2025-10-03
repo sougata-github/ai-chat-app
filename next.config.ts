7a439a9ca1cce83ab4ccf471f0a35a91610c144d
Sougata <sougatadas9874@gmail.com>
2025-10-03 13:40:05 +0530

    modify next-config.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
